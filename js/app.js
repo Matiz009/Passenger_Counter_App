bb4bacd9fb16d58ae48864b6473f4d5c4c4aaddc
MATI UL REHMAN <iammatiz00@gmail.com>
2022-08-10 00:33:53 +0500

    save feature added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 let count = document.getElementById("count"); //initializing variable count
+
 count.innerText = 0; //checking by changing the value
+
+let total = 0; //initializing variable total
+
 let add = document.getElementById("add"); //initializing variable add
 add.addEventListener("click", function() {
     count.innerText = parseInt(count.innerText) + 1; //checking by changing the value
+    total = count; //checking by changing the value
 });
 
 let subtract = document.getElementById("subtract"); //initializing variable subtract
@@ -11,9 +16,16 @@ subtract.addEventListener("click", function() {
     if (count.innerText < 0) {
         count.innerText = 0;
     }
+    total = count; //checking by changing the value
 });
 
 let reset = document.getElementById("reset");
 reset.addEventListener("click", function() {
     count.innerText = 0;
+});
+
+let save = document.getElementById("save");
+save.addEventListener("click", function() {
+    let print = document.getElementById("previous");
+    print.innerText = "Saved Value is : " + total.innerText;
 });
