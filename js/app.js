c7ff84841a0ee806907210503013b3f3eb0258f7
MATI UL REHMAN <iammatiz00@gmail.com>
2022-08-09 14:58:46 +0500

    completed

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
-let count = document.getElementById("count");
-count.innerText = 5;
+let count = document.getElementById("count"); //initializing variable count
+count.innerText = 5; //checking by changing the value
+let add = document.getElementById("add"); //initializing variable add
+add.addEventListener("click", function() {
+    count.innerText = parseInt(count.innerText) + 1; //checking by changing the value
+});
+
+let subtract = document.getElementById("subtract"); //initializing variable subtract
+subtract.addEventListener("click", function() {
+    count.innerText = parseInt(count.innerText) - 1; //checking by changing the value
+    if (count.innerText < 0) {
+        count.innerText = 0;
+    }
+});
+
+let reset = document.getElementById("reset");
+reset.addEventListener("click", function() {
+    count.innerText = 5;
+});
